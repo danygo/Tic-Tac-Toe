b64fcebf9adf613518f01bde141168b54a7f6c41
danygo <senikdaniel@gmail.com>
2020-12-03 14:22:14 +0200

    Add files via upload

--- tic.c
+++ tic.c
@@ -2,99 +2,100 @@
 #include<stdlib.h>
 #include<time.h>
 
+//holds x, o or - if not assigned
+char gameTable[3][3] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
+//getting random number between 0-2
 int getRandomNumber() {
     return rand() % 3;
 }
 
-void showGameTable(char gameTable[3][3]) {
-    for (int i = 0; i <= 2; i++) {
-        for (int j = 0; j <= 2; j++) {
-            if (j == 2) printf("%c\n", gameTable[i][j]);
-            else printf("%c ", gameTable[i][j]);
+//get a free column in a row
+int getFreeColumnInRow(int row, char sign) {
+    if (gameTable[row][0] == sign && gameTable[row][1] == sign && gameTable[row][2] == '-') return 2;
+    else if (gameTable[row][0] == sign && gameTable[row][2] == sign && gameTable[row][1] == '-') return 1;
+    else if (gameTable[row][1] == sign && gameTable[row][2] == sign && gameTable[row][0] == '-') return 0;
+
+    return -1;
+}
+
+//get a free row in a column
+int getFreeRowInColumn(int column, char sign) {
+    if (gameTable[0][column] == sign && gameTable[1][column] == sign && gameTable[2][column] == '-') return 2;
+    else if (gameTable[0][column] == sign && gameTable[2][column] == sign && gameTable[1][column] == '-') return 1;
+    else if (gameTable[1][column] == sign && gameTable[2][column] == sign && gameTable[0][column] == '-') return 0;
+
+    return -1;
+}
+
+//showing the game table in a grid
+void showGameTable() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%c ", gameTable[i][j]);
         }
+        printf("\n");
     }
 }
 
 int main(void) {
-    int playerMoveLine, playerMoveColumn, computerWon = 0, moves = 0, computerMoveLine, computerMoveColumn;
+    int row, column, computerWon = 0, moves = 0;
 
-    char gameTable[3][3];
-    int markedSegments[3][3];
-
-    //initialize arrays
-    for (int i = 0; i <= 2; i++) {
-        for (int j = 0; j <= 2; j++) {
-            gameTable[i][j] = '-';
-            markedSegments[i][j] = 0;
-        }
-    }
-
+    //game loop
     while (computerWon == 0 && moves < 9) {
-        if (moves <= 2) { //first two moves must be in random corners of the gameTable
+        if (moves <= 2) {  //first two moves must be in random corners of the gameTable
+            //get random line and column
             do {
                 srand(time(0));
-                computerMoveLine = getRandomNumber();
-                computerMoveColumn = getRandomNumber();
-            } while ((computerMoveLine == 1 || computerMoveColumn == 1) || markedSegments[computerMoveLine][computerMoveColumn] == 1);
+                row = getRandomNumber();
+                column = getRandomNumber();
+            } while (row == 1 || column == 1 || gameTable[row][column] != '-');
 
             //assign random move
-            gameTable[computerMoveLine][computerMoveColumn] = 'x';
-            markedSegments[computerMoveLine][computerMoveColumn] = 1;
-        } else { //next moves will block the player or win the game for the computer
+            gameTable[row][column] = 'x';
+        } else {  //next moves will block the player or win the game for the computer
             //check for winning in rows
-            for (int k = 0; k <= 2; k++) {
-                if (gameTable[k][0] == 'x' && gameTable[k][1] == 'x' && markedSegments[k][2] == 0) {
-                    gameTable[k][2] = 'x';
-                    computerWon = 1;
-                    break;
-                } else if (gameTable[k][0] == 'x' && gameTable[k][2] == 'x' && markedSegments[k][1] == 0) {
-                    gameTable[k][1] = 'x';
-                    computerWon = 1;
-                    break;
-                } else if (gameTable[k][1] == 'x' && gameTable[k][2] == 'x' && markedSegments[k][0] == 0) {
-                    gameTable[k][0] = 'x';
-                    computerWon = 1;
-                    break;
-                }
+            for (int k = 0; k < 3; k++) {
+                int freeColumnInRow = getFreeColumnInRow(k, 'x');
+
+                if (freeColumnInRow == -1) continue;
+
+                gameTable[k][freeColumnInRow] = 'x';
+                computerWon = 1;
+                break;
             }
 
             if (computerWon == 0) {
                 //check for winning in columns
-                for (int k = 0; k <= 2; k++) {
-                    if (gameTable[0][k] == 'x' && gameTable[1][k] == 'x' && markedSegments[2][k] == 0) {
-                        gameTable[2][k] = 'x';
-                        computerWon = 1;
-                        break;
-                    } else if (gameTable[0][k] == 'x' && gameTable[2][k] == 'x' && markedSegments[1][k] == 0) {
-                        gameTable[1][k] = 'x';
-                        computerWon = 1;
-                        break;
-                    } else if (gameTable[1][k] == 'x' && gameTable[2][k] == 'x' && markedSegments[0][k] == 0) {
-                        gameTable[0][k] = 'x';
-                        computerWon = 1;
-                        break;
-                    }
+                for (int k = 0; k < 3; k++) {
+                    int freeRowInColumn = getFreeRowInColumn(k, 'x');
+
+                    if (freeRowInColumn == -1) continue;
+
+                    gameTable[freeRowInColumn][k] = 'x';
+                    computerWon = 1;
+                    break;
                 }
             }
 
             if (computerWon == 0) {
-                //check for winning diagonal
-                if (gameTable[0][0] == 'x' && gameTable[1][1] == 'x' && markedSegments[2][2] == 0) {
+                //check for winning diagonals
+                if (gameTable[0][0] == 'x' && gameTable[1][1] == 'x' && gameTable[2][2] == '-') {
                     gameTable[2][2] = 'x';
                     computerWon = 1;
-                } else if (gameTable[0][0] == 'x' && gameTable[2][2] == 'x' && markedSegments[1][1] == 0) {
+                } else if (gameTable[0][0] == 'x' && gameTable[2][2] == 'x' && gameTable[1][1] == '-') {
                     gameTable[1][1] = 'x';
                     computerWon = 1;
-                } else if (gameTable[1][1] == 'x' && gameTable[2][2] == 'x' && markedSegments[0][0] == 0) {
+                } else if (gameTable[1][1] == 'x' && gameTable[2][2] == 'x' && gameTable[0][0] == '-') {
                     gameTable[0][0] = 'x';
                     computerWon = 1;
-                } else if (gameTable[0][2] == 'x' && gameTable[1][1] == 'x' && markedSegments[2][0] == 0) {
+                } else if (gameTable[0][2] == 'x' && gameTable[1][1] == 'x' && gameTable[2][0] == '-') {
                     gameTable[2][0] = 'x';
                     computerWon = 1;
-                } else if (gameTable[2][0] == 'x' && gameTable[1][1] == 'x' && markedSegments[0][2] == 0) {
+                } else if (gameTable[2][0] == 'x' && gameTable[1][1] == 'x' && gameTable[0][2] == '-') {
                     gameTable[0][2] = 'x';
                     computerWon = 1;
-                } else if (gameTable[2][0] == 'x' && gameTable[0][2] == 'x' && markedSegments[1][1] == 0) {
+                } else if (gameTable[2][0] == 'x' && gameTable[0][2] == 'x' && gameTable[1][1] == '-') {
                     gameTable[1][1] = 'x';
                     computerWon = 1;
                 }
@@ -102,75 +103,51 @@ int main(void) {
 
             int computerBlock = 0;
 
-            if (computerBlock == 0 && computerWon == 0) {
+            if (computerWon == 0) {
                 //check for blocking in rows
-                for (int k = 0; k <= 2; k++) {
-                    if (gameTable[k][0] == 'o' && gameTable[k][1] == 'o' && markedSegments[k][2] == 0) {
-                        gameTable[k][2] = 'x';
-                        markedSegments[k][2] = 1;
-                        computerBlock = 1;
-                        break;
-                    } else if (gameTable[k][0] == 'o' && gameTable[k][2] == 'o' && markedSegments[k][1] == 0) {
-                        gameTable[k][1] = 'x';
-                        markedSegments[k][1] = 1;
-                        computerBlock = 1;
-                        break;
-                    } else if (gameTable[k][1] == 'o' && gameTable[k][2] == 'o' && markedSegments[k][0] == 0) {
-                        gameTable[k][0] = 'x';
-                        markedSegments[k][0] = 1;
-                        computerBlock = 1;
-                        break;
-                    }
+                for (int k = 0; k < 3; k++) {
+                    int freeColumnInRow = getFreeColumnInRow(k, 'o');
+
+                    if (freeColumnInRow == -1) continue;
+
+                    gameTable[k][freeColumnInRow] = 'x';
+                    computerBlock = 1;
+                    break;
                 }
             }
 
             if (computerBlock == 0 && computerWon == 0) {
                 //check for blocking in columns
-                for (int k = 0; k <= 2; k++) {
-                    if (gameTable[0][k] == 'o' && gameTable[1][k] == 'o' && markedSegments[2][k] == 0) {
-                        gameTable[2][k] = 'x';
-                        markedSegments[2][k] = 1;
-                        computerBlock = 1;
-                        break;
-                    } else if (gameTable[0][k] == 'o' && gameTable[2][k] == 'o' && markedSegments[1][k] == 0) {
-                        gameTable[1][k] = 'x';
-                        markedSegments[1][k] = 1;
-                        computerBlock = 1;
-                        break;
-                    } else if (gameTable[1][k] == 'o' && gameTable[2][k] == 'o' && markedSegments[0][k] == 0) {
-                        gameTable[0][k] = 'x';
-                        markedSegments[0][k] = 1;
-                        computerBlock = 1;
-                        break;
-                    }
+                for (int k = 0; k < 3; k++) {
+                    int freeRowInColumn = getFreeRowInColumn(k, 'o');
+
+                    if (freeRowInColumn == -1) continue;
+
+                    gameTable[freeRowInColumn][k] = 'x';
+                    computerBlock = 1;
+                    break;
                 }
             }
 
             if (computerBlock == 0 && computerWon == 0) {
-                //check for blocking in diagonal
-                if (gameTable[0][0] == 'o' && gameTable[1][1] == 'o' && markedSegments[2][2] == 0) {
+                //check for blocking in diagonals
+                if (gameTable[0][0] == 'o' && gameTable[1][1] == 'o' && gameTable[2][2] == '-') {
                     gameTable[2][2] = 'x';
-                    markedSegments[2][2] = 1;
                     computerBlock = 1;
-                } else if (gameTable[0][0] == 'o' && gameTable[2][2] == 'o' && markedSegments[1][1] == 0) {
+                } else if (gameTable[0][0] == 'o' && gameTable[2][2] == 'o' && gameTable[1][1] == '-') {
                     gameTable[1][1] = 'x';
-                    markedSegments[1][1] = 1;
                     computerBlock = 1;
-                } else if (gameTable[1][1] == 'o' && gameTable[2][2] == 'o' && markedSegments[0][0] == 0) {
+                } else if (gameTable[1][1] == 'o' && gameTable[2][2] == 'o' && gameTable[0][0] == '-') {
                     gameTable[0][0] = 'x';
-                    markedSegments[0][0] = 1;
                     computerBlock = 1;
-                } else if (gameTable[0][2] == 'o' && gameTable[1][1] == 'o' && markedSegments[2][0] == 0) {
+                } else if (gameTable[0][2] == 'o' && gameTable[1][1] == 'o' && gameTable[2][0] == '-') {
                     gameTable[2][0] = 'x';
-                    markedSegments[2][0] = 1;
                     computerBlock = 1;
-                } else if (gameTable[2][0] == 'o' && gameTable[1][1] == 'o' && markedSegments[0][2] == 0) {
+                } else if (gameTable[2][0] == 'o' && gameTable[1][1] == 'o' && gameTable[0][2] == '-') {
                     gameTable[0][2] = 'x';
-                    markedSegments[0][2] = 1;
                     computerBlock = 1;
-                } else if (gameTable[2][0] == 'o' && gameTable[0][2] == 'o' && markedSegments[1][1] == 0) {
+                } else if (gameTable[2][0] == 'o' && gameTable[0][2] == 'o' && gameTable[1][1] == '-') {
                     gameTable[1][1] = 'x';
-                    markedSegments[1][1] = 1;
                     computerBlock = 1;
                 }
             }
@@ -181,20 +158,19 @@ int main(void) {
                     //if the last move is not winning or blocking assign the last element to x, so random do while loop is not executed 
                     for (int i = 0; i <= 2; i++) {
                         for (int j = 0; j <= 2; j++) {
-                            if (markedSegments[i][j] == 0) gameTable[i][j] = 'x';
+                            if (gameTable[i][j] == '-') gameTable[i][j] = 'x';
                         }
                     }
                 } else {
                     //get random move from the computer if no winning or blocking sequence was found 
                     do {
                         srand(time(0));
-                        computerMoveLine = getRandomNumber();
-                        computerMoveColumn = getRandomNumber();
-                    } while (markedSegments[computerMoveLine][computerMoveColumn] == 1);
+                        row = getRandomNumber();
+                        column = getRandomNumber();
+                    } while (gameTable[row][column] != '-');
 
                     //assign random move
-                    gameTable[computerMoveLine][computerMoveColumn] = 'x';
-                    markedSegments[computerMoveLine][computerMoveColumn] = 1;
+                    gameTable[row][column] = 'x';
                 }
             }
         }
@@ -205,16 +181,15 @@ int main(void) {
         //showing the gameTable
         showGameTable(gameTable);
 
-        //checking for the player to give correct input for his move, no need to check if player wins
+        //checking for the player to give correct input for his move
         if (computerWon == 0 && moves <= 7) {
             do {
-                scanf("%d", &playerMoveLine);
-                scanf("%d", &playerMoveColumn);
-            } while (markedSegments[playerMoveLine][playerMoveColumn] == 1 || playerMoveLine < 0 || playerMoveLine > 2 || playerMoveColumn < 0 || playerMoveColumn > 2);
+                scanf("%d", &row);
+                scanf("%d", &column);
+            } while (gameTable[row][column] != '-' || row < 0 || row > 2 || column < 0 || column > 2);
 
             //assign player move
-            gameTable[playerMoveLine][playerMoveColumn] = 'o';
-            markedSegments[playerMoveLine][playerMoveColumn] = 1;
+            gameTable[row][column] = 'o';
 
             //incrementing moves
             moves++;
